0fe16e199a069332fb2ce78b7f016a7440c5bde7
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-08-03 16:08:20 +0200

    Copy Utopia.dll in Sandbox builddir & some modifications

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-set_project("Utopia")
+set_project("UtopiaGameEngine")
 set_version("0.0.0")
 
 add_rules("mode.debug", "mode.release")
@@ -6,17 +6,13 @@ set_languages("cxx17")
 
 add_requires("spdlog v1.9.0") -- latest version at the time
 
-
-outputdir = "$(mode)-$(os)-$(arch)"
-
+local outputdir = "$(mode)-$(os)-$(arch)"
 
 target("Utopia")
     set_kind("shared")
-    set_basename("Utopia")
 
     set_targetdir("/build/" .. outputdir .. "/Utopia/bin")
     set_objectdir("/build/" .. outputdir .. "/Utopia/obj")
-    
 
     add_files("Utopia/src/**.cpp")
     add_headerfiles("Utopia/src/**.h")
@@ -30,7 +26,6 @@ target("Utopia")
 
 target("Sandbox")
     set_kind("binary")
-    set_basename("Sandbox")
 
     set_targetdir("/build/" .. outputdir .. "/SandBox/bin")
     set_objectdir("/build/" .. outputdir .. "/SandBox/obj")
@@ -44,3 +39,7 @@ target("Sandbox")
     if is_plat("windows") then
         add_defines("UT_PLATFORM_WINDOWS")
     end
+
+    after_build(function (target)
+        os.cp("/build/" .. outputdir .. "/Utopia/bin/Utopia.dll", "/build/" .. outputdir .. "/SandBox/bin")
+    end)
