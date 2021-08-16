b559e833c8d146e5ca225d6f42ea819094169edc
TNtube <51389578+TNtube@users.noreply.github.com>
2021-08-16 14:39:44 -0300

    replace 4 spaces by tabs du to a miss configuration of CLion second part

--- SandBox/src/SandBoxApp.cpp
+++ SandBox/src/SandBoxApp.cpp
@@ -34,8 +34,8 @@ public:
 	~SandBox() override = default;
 
 private:
-    std::unique_ptr<Utopia::Layer> layer = std::make_unique<LayerTest>("First Layer");
-    std::unique_ptr<Utopia::Layer> layer2 = std::make_unique<LayerTest>("Second Layer");
+	std::unique_ptr<Utopia::Layer> layer = std::make_unique<LayerTest>("First Layer");
+	std::unique_ptr<Utopia::Layer> layer2 = std::make_unique<LayerTest>("Second Layer");
 	std::unique_ptr<Utopia::Layer> imGuiLayer = std::make_unique<Utopia::ImGuiLayer>();
 };
 
