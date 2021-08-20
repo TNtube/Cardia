badede71db055bec579a32aef13be2eaca051be9
TNtube <51389578+TNtube@users.noreply.github.com>
2021-08-19 21:42:09 -0300

    Adding comment to the triangle to specify that it's old OpenGL

--- Utopia/src/Platform/OpenGL/OpenGLContext.cpp
+++ Utopia/src/Platform/OpenGL/OpenGLContext.cpp
@@ -16,6 +16,7 @@ void Utopia::OpenGLContext::init()
 
 void Utopia::OpenGLContext::swapBuffers()
 {
+	// OLD OPENGL PRACTICES, JUST USED AS AN EXAMPLE
 	glBegin(GL_TRIANGLES);
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex2f(-0.5f, -0.5f);
