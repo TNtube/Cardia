751f1d7f1115ed10f7ed2d58cc27a902d0bb3b2f
TNtube <tntube.dev@gmail.com>
2022-05-26 17:19:52 +0200

    Fix SceneSerializer

--- Cardia/include/Cardia/SerializationUtils/SceneSerializer.hpp
+++ Cardia/include/Cardia/SerializationUtils/SceneSerializer.hpp
@@ -2,41 +2,10 @@
 
 #include "Cardia/ECS/Scene.hpp"
 #include "Cardia/ECS/Entity.hpp"
-#include "Cardia/ECS/Components.hpp"
-
-#include <json/json.h>
 
 namespace Cardia::SerializerUtils
 {
-        inline std::string SerializeScene(Scene* scene)
-        {
-                Json::Value root;
-                const auto view = scene->getRegistry().view<Component::Name>();
-
-                for (const auto entity_id : view)
-                {
-                        Entity entity(entity_id, scene);
-                        const auto name = entity.getComponent<Component::Name>();
-
-                        // Transform
-                        const auto& transform = entity.getComponent<Component::Transform>();
-                        root[name.name]["position"]["x"] = static_cast<double>(transform.position.x);
-                        root[name.name]["position"]["y"] = static_cast<double>(transform.position.y);
-                        root[name.name]["position"]["z"] = static_cast<double>(transform.position.z);
-                        root[name.name]["rotation"]["x"] = static_cast<double>(transform.rotation.x);
-                        root[name.name]["rotation"]["y"] = static_cast<double>(transform.rotation.y);
-                        root[name.name]["rotation"]["z"] = static_cast<double>(transform.rotation.z);
-                        root[name.name]["scale"]["x"] = static_cast<double>(transform.scale.x);
-                        root[name.name]["scale"]["y"] = static_cast<double>(transform.scale.y);
-                        root[name.name]["scale"]["z"] = static_cast<double>(transform.scale.z);
-                }
-
-                std::stringstream output;
-
-                output << root;
-                
-                return output.str();
-        }
+        std::string SerializeScene(Scene* scene);
 
         Scene DeserializeScene(const std::string& serializedScene);
 }
