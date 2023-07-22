72350ebe9cb111c66bc446f254685faaee6b5044
Stanis Kapusta <51389578+TNtube@users.noreply.github.com>
2023-07-23 01:15:07 +0200

    Add temporary shader build script

---
+++ scripts/BuildSimpleShader.bat
@@ -0,0 +1,2 @@
+glslc ../CardiaTor/resources/shaders/simple.frag -o ../CardiaTor/resources/shaders/simple.frag.spv
+glslc ../CardiaTor/resources/shaders/simple.vert -o ../CardiaTor/resources/shaders/simple.vert.spv
